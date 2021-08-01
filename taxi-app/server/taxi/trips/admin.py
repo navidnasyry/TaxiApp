71add1281c0ea6367898c53c7b8567b2939bfae8
Navid <navidnasyry@gmail.com>
2021-08-01 11:22:01 +0430

    add admin setting

--- taxi-app/server/taxi/trips/admin.py
+++ taxi-app/server/taxi/trips/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import User
 # Register your models here.
+
+
+
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    pass
+
+
