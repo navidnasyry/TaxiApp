ead0719d0e4617176cb50a69c71de36fed794e46
Navid <navidnasyry@gmail.com>
2021-08-02 12:55:52 +0430

    add login and signup test

--- taxi-app/server/taxi/taxi/urls.py
+++ taxi-app/server/taxi/taxi/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from trips.views import SignUpView, LogInView
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/sign_up/', SignUpView.as_view(), name='sign_up'),
+    path('api/log_in/', LogInView.as_view(), name='log_in'), # new
+    path('api/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'), # new
 ]
--- taxi-app/server/taxi/trips/views.py
+++ taxi-app/server/taxi/trips/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from .serializers import UserSerializer, LogInSerializer
+
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class LogInView(TokenObtainPairView):
+    serializer_class = LogInSerializer
+
+    
+
+
+
