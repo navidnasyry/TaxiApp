535604df06945fb199647fb2c2e19d661c7dd76e
Navid Nasiri <nvd.nasiri@ec.iut.ac.ir>
2021-08-11 23:49:35 +0430

    end _v1 _ 6 failed

--- taxi-app/server/taxi/taxi/routing.py
+++ taxi-app/server/taxi/taxi/routing.py
@@ -3,12 +3,12 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from trips.consumers import TaxiConsumer
 
-
+from taxi.middleware import TokenAuthMiddlewareStack
 
 application = ProtocolTypeRouter({
-    'websocket':URLRouter([
-        path('taxi/', TaxiConsumer),
-    ]),
-
-
+    'websocket': TokenAuthMiddlewareStack( 
+        URLRouter([
+            path('taxi/', TaxiConsumer),
+        ])
+    ),
 })
--- taxi-app/server/taxi/trips/consumers.py
+++ taxi-app/server/taxi/trips/consumers.py
@@ -3,13 +3,36 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
 class TaxiConsumer(AsyncJsonWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
-    
+    async def connect(self): 
+        user = self.scope['user']
+        if user.is_anonymous:
+            await self.close()
+        else:
+            await self.channel_layer.group_add(
+                group='test',
+                channel=self.channel_name
+            )
+            await self.accept()
 
-    async def disconnect(self, code):
+
+            
+    async def receive_json(self, content, **kwargs):
+        message_type = content.get('type')
+        if message_type == 'echo.message':
+            await self.send_json({
+                'type': message_type,
+                'data': content.get('data'),
+            })
+
+    async def echo_message(self, message):
+        await self.send_json({
+            'type': message.get('type'),
+            'data': message.get('data'),
+        })
+
+    async def disconnect(self, code): # changed
+        await self.channel_layer.group_discard(
+            group='test',
+            channel=self.channel_name
+        )
         await super().disconnect(code)
-        
-
-
-
